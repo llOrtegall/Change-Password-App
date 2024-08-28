533f8136608eb1c2c9d571bca8441508ff2003df
llOrtegall <IvanOrtega_97@hotmail.com>
2024-08-28 12:35:41 -0500

    chore: Update nginx.conf to add proxy configuration for /api endpoint and update src/main.tsx to use '/api' as the base URL for API requests

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,11 @@
 import { createRoot } from 'react-dom/client'
 import { StrictMode } from 'react'
 import App from './App.tsx'
-import axios from 'axios'
 import './index.css'
+import axios from 'axios'
 
-axios.defaults.baseURL = import.meta.env.VITE_URL_API as string
+axios.defaults.baseURL = '/api'
 axios.defaults.withCredentials = true
-
-console.log(import.meta.env.VITE_URL_API);
-
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
